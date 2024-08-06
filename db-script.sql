96afd28904fce268b2ab2aaadd2949089ee4a820
prajeethWolf <prajeeth31@gmail.com>
2024-08-06 19:22:10 +0530

    add 10 student records

--- db-script.sql
+++ db-script.sql
@@ -2,3 +2,15 @@ CREATE TABLE student (
     id VARCHAR(5) PRIMARY KEY ,
     name VARCHAR(20) NOT NULL ,
 );
+
+INSERT INTO student VALUES
+                        ('C001', 'stud-1'),
+                        ('C002', 'stud-2'),
+                        ('C003', 'stud-3'),
+                        ('C004', 'stud-4'),
+                        ('C005', 'stud-5'),
+                        ('C006', 'stud-6'),
+                        ('C007', 'stud-7'),
+                        ('C008', 'stud-8'),
+                        ('C009', 'stud-9'),
+                        ('C010', 'stud-10');
