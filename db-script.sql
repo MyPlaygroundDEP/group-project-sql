175243226e19ca6eaa32d4c61076e65b8584a74c
prajeethWolf <prajeeth31@gmail.com>
2024-08-06 19:50:11 +0530

    recorrection done to batch relation db-script.sql

--- db-script.sql
+++ db-script.sql
@@ -1,16 +1,8 @@
-CREATE TABLE student (
-    id VARCHAR(5) PRIMARY KEY ,
-    name VARCHAR(20) NOT NULL ,
+CREATE TABLE batch (
+                       id VARCHAR(5) PRIMARY KEY ,
+                       heads_count INT(100),
+                       program_id VARCHAR(50) NOT NULL
 );
 
-INSERT INTO student VALUES
-                        ('C001', 'stud-1'),
-                        ('C002', 'stud-2'),
-                        ('C003', 'stud-3'),
-                        ('C004', 'stud-4'),
-                        ('C005', 'stud-5'),
-                        ('C006', 'stud-6'),
-                        ('C007', 'stud-7'),
-                        ('C008', 'stud-8'),
-                        ('C009', 'stud-9'),
-                        ('C010', 'stud-10');
+INSERT INTO batch VALUES
+                        ('B001', 'stud-1'),
