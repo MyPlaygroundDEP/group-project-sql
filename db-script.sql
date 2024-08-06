3172d19fdd57359d2e56a0717fac1299ef6fcdf2
udeeka <cudeeka@gmail.com>
2024-08-06 19:06:15 +0530

    Add 10 exam records

--- db-script.sql
+++ db-script.sql
@@ -1,32 +1,21 @@
-CREATE TABLE module
-(
-    id      VARCHAR(10) PRIMARY KEY,
-    name    VARCHAR(100),
-    credits INT
-);
-
-CREATE TABLE program_module
-(
-    program_id VARCHAR(10) NOT NULL ,
-    module_id VARCHAR(10) NOT NULL ,
-    CONSTRAINT fk_program_id FOREIGN KEY (program_id) REFERENCES program(id),
-    CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module(id),
-    CONSTRAINT pk_program_module PRIMARY KEY (program_id,module_id)
-);
-
-INSERT INTO program (id,name) VALUES ('P001', 'CMJD'),('P002', 'GDSE'),('P003', 'DEP');
-CREATE TABLE program
-(
-    id   VARCHAR(10) PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
-
 CREATE TABLE exam(
     exam_code VARCHAR(10) PRIMARY KEY ,
     passing_score INT NOT NULL ,
     module_id VARCHAR(10) NOT NULL ,
     CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module(id)
 );
+INSERT INTO exam (exam_code, passing_score, module_id) VALUES
+                                                           ('EXM001', 50, 'MOD001'),
+                                                           ('EXM002', 60, 'MOD002'),
+                                                           ('EXM003', 55, 'MOD003'),
+                                                           ('EXM004', 70, 'MOD004'),
+                                                           ('EXM005', 65, 'MOD005'),
+                                                           ('EXM006', 75, 'MOD006'),
+                                                           ('EXM007', 80, 'MOD007'),
+                                                           ('EXM008', 85, 'MOD008'),
+                                                           ('EXM009', 60, 'MOD009'),
+                                                           ('EXM010', 50, 'MOD010');
+
 
 CREATE TABLE student (
     id VARCHAR(5) PRIMARY KEY ,
