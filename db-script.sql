22e6833ea51820712f227c65ca630835f742e68c
udeeka <cudeeka@gmail.com>
2024-08-06 19:39:40 +0530

    Create student-exam relation

--- db-script.sql
+++ db-script.sql
@@ -1,8 +1,13 @@
+CREATE TABLE program
+(
+    id   VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
 CREATE TABLE exam(
-    exam_code VARCHAR(10) PRIMARY KEY ,
-    passing_score INT NOT NULL ,
-    module_id VARCHAR(10) NOT NULL ,
-    CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module(id)
+                     exam_code VARCHAR(10) PRIMARY KEY ,
+                     passing_score INT NOT NULL ,
+                     module_id VARCHAR(10) NOT NULL ,
+                     CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module(id)
 );
 INSERT INTO exam (exam_code, passing_score, module_id) VALUES
                                                            ('EXM001', 50, 'MOD001'),
@@ -15,3 +20,12 @@ INSERT INTO exam (exam_code, passing_score, module_id) VALUES
                                                            ('EXM008', 85, 'MOD008'),
                                                            ('EXM009', 60, 'MOD009'),
                                                            ('EXM010', 50, 'MOD010');
+
+CREATE TABLE student_exam(
+    exam_code VARCHAR(10) NOT NULL ,
+    student_id VARCHAR(10) NOT NULL ,
+    marks INT NOT NULL ,
+    CONSTRAINT fk_exam_code FOREIGN KEY (exam_code) REFERENCES exam(exam_code),
+    CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES student(id)
+
+);
