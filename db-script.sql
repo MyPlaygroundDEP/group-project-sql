ada41262b487ac1fb2a62392042eca5f9326b958
prajeethWolf <prajeeth31@gmail.com>
2024-08-06 19:45:04 +0530

    add db-script for batch relation

--- db-script.sql
+++ db-script.sql
@@ -1,16 +1,7 @@
-CREATE TABLE student (
+CREATE TABLE batch (
     id VARCHAR(5) PRIMARY KEY ,
-    name VARCHAR(20) NOT NULL ,
+     VARCHAR(20) NOT NULL ,
+    heads_count INT(100),
+    program_id VARCHAR(50) NOT NULL
 );
 
-INSERT INTO student VALUES
-                        ('C001', 'stud-1'),
-                        ('C002', 'stud-2'),
-                        ('C003', 'stud-3'),
-                        ('C004', 'stud-4'),
-                        ('C005', 'stud-5'),
-                        ('C006', 'stud-6'),
-                        ('C007', 'stud-7'),
-                        ('C008', 'stud-8'),
-                        ('C009', 'stud-9'),
-                        ('C010', 'stud-10');
