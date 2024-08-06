dcbb88081998a51c64201c2acf40c3b9e3386025
prajeethWolf <prajeeth31@gmail.com>
2024-08-06 20:07:41 +0530

    Add Enrolement Relation

--- db-script.sql
+++ db-script.sql
@@ -1,13 +1,7 @@
-CREATE TABLE batch (
-                       id VARCHAR(5) PRIMARY KEY ,
-                       heads_count INT(100),
-                       program_id VARCHAR(50) NOT NULL
+CREATE TABLE enrolment
+(
+    st_id      VARCHAR(5) PRIMARY KEY,
+    program_id VARCHAR(5) NOT NULL,
+    CONSTRAINT pk_enrolment PRIMARY KEY (st_id, program_id)
 );
 
-INSERT INTO batch VALUES
-                        ('B001', 'stud-1', 'Prog-1'),
-                        ('B002', 'stud-2', 'Prog-2'),
-                        ('B003', 'stud-3', 'Prog-3'),
-                        ('B004', 'stud-4', 'Prog-3'),
-                        ('B005', 'stud-5', 'Prog-3'),
-                        ('B006', 'stud-6', 'Prog-1');
