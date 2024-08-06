9a6dde9fb7166d88a666d1c6feac533d9a2628f8
udeeka <cudeeka@gmail.com>
2024-08-06 19:06:15 +0530

    Add 10 exam records

--- db-script.sql
+++ db-script.sql
@@ -4,3 +4,14 @@ CREATE TABLE exam(
     module_id VARCHAR(10) NOT NULL ,
     CONSTRAINT fk_module_id FOREIGN KEY (module_id) REFERENCES module(id)
 );
+INSERT INTO exam (exam_code, passing_score, module_id) VALUES
+                                                           ('EXM001', 50, 'MOD001'),
+                                                           ('EXM002', 60, 'MOD002'),
+                                                           ('EXM003', 55, 'MOD003'),
+                                                           ('EXM004', 70, 'MOD004'),
+                                                           ('EXM005', 65, 'MOD005'),
+                                                           ('EXM006', 75, 'MOD006'),
+                                                           ('EXM007', 80, 'MOD007'),
+                                                           ('EXM008', 85, 'MOD008'),
+                                                           ('EXM009', 60, 'MOD009'),
+                                                           ('EXM010', 50, 'MOD010');
